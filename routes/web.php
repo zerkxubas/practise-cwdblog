45cf458eefc612c5726cffacc0457c4780fd6cd4
Subash Sharma <zerkxubas@gmail.com>
2023-11-23 14:40:35 +0545

    google & github authentication added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\ProviderController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/auth/{provider}/redirect', [ProviderController::class,'redirect'])->name('auth.redirect');
+
+Route::get('/auth/{provider}/callback', [ProviderController::class,'callback'])->name('');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
